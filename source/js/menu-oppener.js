58c94581bacbc47fd10b9f0c8ba816bee5fe3691
StasRomanov <stasromanov801@gmail.com>
2019-07-30 20:55:14 +0300

    js

--- source/js/menu-oppener.js
+++ source/js/menu-oppener.js
@@ -4,7 +4,6 @@ var navigation = document.querySelector(".main-navigation");
 navigation.classList.add('main-navigation--close');
 menuOpener.addEventListener("click", function (evt) {
   evt.preventDefault();
-  menuOpener.removeAttribute('href');
   if (menuOpener.classList.contains('main-header__menu-opener--open')){
     menuOpener.classList.add('main-header__menu-opener--close');
   } else {
