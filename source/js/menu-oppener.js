d0f1cc11521c50799968bd578d6cebce1633ce17
StasRomanov <stasromanov801@gmail.com>
2019-08-01 23:18:42 +0300

    header правки

--- source/js/menu-oppener.js
+++ source/js/menu-oppener.js
@@ -1,23 +1,23 @@
-var menuOpener = document.querySelector(".main-navigation__menu-opener");
-var navigation = document.querySelectorAll(".main-navigation__list");
-var navigationClose = "main-navigation--close";
-var svgOpenMenu = "main-navigation__menu-opener--open";
-var svgCloseMenu = "main-navigation__menu-opener--close";
-
 window.onload = function () {
-  for (var i = 0; i<navigation.length; i++) {
-    navigation[i].classList.toggle(navigationClose);
-  }
-  menuOpener.addEventListener("click", function (evt) {
+  var menuOpenerEl = document.querySelector(".main-navigation__menu-opener");
+  var navigation = document.querySelectorAll(".main-navigation__list");
+  var navigationClose = "main-navigation--close";
+  var svgOpenMenuClass = "main-navigation__menu-opener--open";
+  var svgCloseMenuClass = "main-navigation__menu-opener--close";
+
+  navigation.forEach(function(navList) {
+    navList.classList.toggle(navigationClose);
+  });
+  menuOpenerEl.addEventListener("click", function (evt) {
     evt.preventDefault();
-    if (menuOpener.classList.contains(svgOpenMenu)){
-      menuOpener.classList.add(svgCloseMenu);
+    if (menuOpenerEl.classList.contains(svgOpenMenuClass)){
+      menuOpenerEl.classList.add(svgCloseMenuClass);
     } else {
-      menuOpener.classList.remove(svgCloseMenu);
+      menuOpenerEl.classList.remove(svgCloseMenuClass);
     }
-    menuOpener.classList.toggle(svgOpenMenu);
-    for (var i = 0; i < navigation.length; i++) {
-      navigation[i].classList.toggle(navigationClose);
-    }
+    menuOpenerEl.classList.toggle(svgOpenMenuClass);
+    navigation.forEach(function(navList) {
+      navList.classList.toggle(navigationClose);
+    });
   });
 };
