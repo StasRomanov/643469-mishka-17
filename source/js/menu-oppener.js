ca73db20eb0c5fb6c0ec0e8d1c4d05b1b0ccf416
StasRomanov <stasromanov801@gmail.com>
2019-08-09 16:27:24 +0300

    Ymap

--- source/js/menu-oppener.js
+++ source/js/menu-oppener.js
@@ -4,6 +4,7 @@ window.onload = function () {
   var navigationClose = "main-navigation--close";
   var svgOpenMenuClass = "main-navigation__menu-opener--open";
   var svgCloseMenuClass = "main-navigation__menu-opener--close";
+  var animationMenu = "main-navigation__list-animation";
 
   navigation.forEach(function(navList) {
     navList.classList.toggle(navigationClose);
@@ -12,8 +13,14 @@ window.onload = function () {
     evt.preventDefault();
     if (menuOpenerEl.classList.contains(svgOpenMenuClass)){
       menuOpenerEl.classList.add(svgCloseMenuClass);
+      navigation.forEach(function(navList) {
+        navList.classList.toggle(animationMenu);
+      });
     } else {
       menuOpenerEl.classList.remove(svgCloseMenuClass);
+      navigation.forEach(function(navList) {
+        navList.classList.toggle(animationMenu);
+      });
     }
     menuOpenerEl.classList.toggle(svgOpenMenuClass);
     navigation.forEach(function(navList) {
