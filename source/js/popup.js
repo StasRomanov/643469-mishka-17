03217a62b62ed59f8d2d79884c3ccabb76669b60
StasRomanov <stasromanov801@gmail.com>
2019-08-26 12:41:07 +0300

    popup

--- source/js/popup.js
+++ source/js/popup.js
@@ -1,15 +1,21 @@
+"use strict";
 (function () {
   var popupOpen = document.querySelector('.product-week__link');
   var popup = document.querySelector('.popup__wrapper');
+  var popupForm = document.querySelector('.popup');
 
   popupOpen.addEventListener('click', function (evt) {
     evt.preventDefault();
     popup.classList.remove('popup__wrapper--hidden')
   });
-
-  document.addEventListener('keydown',function (e) {
-    if (e.keyCode === 27) {
+  document.addEventListener('keydown', function(event) {
+    if (event.code === 'Escape') {
       popup.classList.add('popup__wrapper--hidden');
     }
   });
+  popup.addEventListener('click', function(e){
+    if (!popupForm.contains(e.target)){
+      popup.classList.add('popup__wrapper--hidden')
+    }
+  })
 })();
