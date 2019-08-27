3cd4a4c4ddeada0bb3f4e004aaa13a1b0aec5c77
StasRomanov <stasromanov801@gmail.com>
2019-08-27 11:03:08 +0300

    правки к защите 1

--- source/js/menu-opener.js
+++ source/js/menu-opener.js
@@ -6,6 +6,7 @@
   var svgOpenMenuClass = "main-navigation__menu-opener--open";
   var svgCloseMenuClass = "main-navigation__menu-opener--close";
   var animationMenu = "main-navigation__list-animation";
+  var animationMenuClose = "main-navigation__list-animation-close";
 
   navigation.forEach(function(navList) {
     navList.classList.toggle(navigationClose);
@@ -16,16 +17,18 @@
       menuOpenerEl.classList.add(svgCloseMenuClass);
       navigation.forEach(function(navList) {
         navList.classList.toggle(animationMenu);
+        navList.classList.remove(animationMenuClose);
       });
     } else {
       menuOpenerEl.classList.remove(svgCloseMenuClass);
       navigation.forEach(function(navList) {
         navList.classList.toggle(animationMenu);
+        navList.classList.add(animationMenuClose);
       });
     }
     menuOpenerEl.classList.toggle(svgOpenMenuClass);
     navigation.forEach(function(navList) {
-      navList.classList.toggle(navigationClose);
+      navList.classList.remove(navigationClose);
     });
   });
 })();
