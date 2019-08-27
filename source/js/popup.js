bdd9d29f022f3487211a0a93ab544ddfc62e4d6a
StasRomanov <stasromanov801@gmail.com>
2019-08-27 11:27:46 +0300

    popup scroll

--- source/js/popup.js
+++ source/js/popup.js
@@ -3,19 +3,29 @@
   var popupOpen = document.querySelector('.product-week__link');
   var popup = document.querySelector('.popup__wrapper');
   var popupForm = document.querySelector('.popup');
-
   popupOpen.addEventListener('click', function (evt) {
     evt.preventDefault();
+    let scrollX = window.scrollX;
+    let scrollY = window.scrollY;
+    window.onscroll = function () {
+      window.scrollTo(scrollX, scrollY);
+    };
     popup.classList.remove('popup__wrapper--hidden')
   });
   document.addEventListener('keydown', function(event) {
     if (event.code === 'Escape') {
       popup.classList.add('popup__wrapper--hidden');
+      window.onscroll = function () {
+        window.scrollTo(scrollX, scrollY);
+      };
     }
   });
   popup.addEventListener('click', function(e){
     if (!popupForm.contains(e.target)){
-      popup.classList.add('popup__wrapper--hidden')
+      popup.classList.add('popup__wrapper--hidden');
+      window.onscroll = function () {
+        window.scrollTo(scrollX, scrollY);
+      };
     }
-  })
+  });
 })();
